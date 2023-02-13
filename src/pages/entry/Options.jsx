cb17bf10ddf1540e054b91eb634005d2a40c7fe5
KarolinaFilipiuk <kfilipiuk@student.agh.edu.pl>
2023-02-13 18:36:48 +0100

    Test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
@@ -14,8 +16,13 @@ const Options = ({ optionType }) => {
       .then((response) => setItems(response.data))
       .catch((error) => {
         // TODO: handle error response
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
