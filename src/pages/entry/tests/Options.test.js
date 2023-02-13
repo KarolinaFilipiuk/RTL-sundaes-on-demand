ebf01abd1767a1aec7c06cb6bbad6a9a8dab02ed
KarolinaFilipiuk <kfilipiuk@student.agh.edu.pl>
2023-02-13 17:59:20 +0100

    Test and code ToppingOption component

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -13,3 +13,22 @@ test('displays image for each scoop option from server', async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('display image for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingsImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingsImages).toHaveLength(3);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = toppingsImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
