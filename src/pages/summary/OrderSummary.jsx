514aefe46060952bbfcbda67c0328f196de414cf
KarolinaFilipiuk <kfilipiuk@student.agh.edu.pl>
2023-02-14 21:05:45 +0100

    Test and code conditional toppings

--- src/pages/summary/OrderSummary.jsx
+++ src/pages/summary/OrderSummary.jsx
@@ -12,16 +12,26 @@ const OrderSummary = ({ setOrderPhase }) => {
     </li>
   ));
 
-  const toppingsArray = Object.keys(optionCounts.toppings); // [["M&M"], ["Gummi bears"]]
-  const toppingsList = toppingsArray.map((key) => <li key={key}>{key}</li>);
+  // only display toppings if the toppings total is nonzero
+  const hasToppings = totals.toppings > 0;
+  let toppingsDisplay = null;
 
+  if (hasToppings) {
+    const toppingsArray = Object.keys(optionCounts.toppings);
+    const toppingsList = toppingsArray.map((key) => <li key={key}>{key}</li>);
+    toppingsDisplay = (
+      <>
+        <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+        <ul>{toppingsList}</ul>
+      </>
+    );
+  }
   return (
     <div>
       <h1>Order Summary</h1>
       <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
       <ul>{scoopsList}</ul>
-      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
-      <ul>{toppingsList}</ul>
+      {toppingsDisplay}
       <SummaryForm setOrderPhase={setOrderPhase} />
     </div>
   );
